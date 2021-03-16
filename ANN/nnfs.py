bd4ccb69d643c7387d9b4d3fe65c1d655d47ad5e
Rakibul Hassan Rony <2018000000012@seu.edu.bd>
2021-03-16 23:37:36 +0600

    two layer calculation

--- ANN/nnfs.py
+++ ANN/nnfs.py
@@ -6,13 +6,23 @@ weights = [[0.2, 0.8, -0.5, 1.0],
           [-0.26, -0.27, 0.17, 0.87]]
 
 biases = [2, 3, 0.5]
+
+weights2 = [[0.1, -0.14, 0.5],
+          [-0.5, 0.12, -0.33],
+          [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
 layer_outputs = []
 
 # let's try with numpy
 import numpy as np
 # calculating matrix multiplication via np.dot(x,y)
-outputs = np.dot(inputs,np.array(weights).T)+biases 
-print(outputs) # [4.8   1.21  2.385]
+layer1_outputs = np.dot(inputs,np.array(weights).T)+biases 
+layer2_outputs = np.dot(layer1_outputs,np.array(weights2).T)+biases2
+# print(outputs) # [4.8   1.21  2.385]
+# print(layer1_outputs)
+print(layer2_outputs)
 
 
 
